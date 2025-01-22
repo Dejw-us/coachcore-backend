67e674b8b04832a4a12ab0769707163f1326aaa4
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-01-22 09:22:36 +0100

    Added main class

---
+++ training-api/src/main/java/pro/shapeit/training/TrainingApiApplication.java
@@ -0,0 +1,11 @@
+package pro.shapeit.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TrainingApiApplication.class, args);
+  }
+}
