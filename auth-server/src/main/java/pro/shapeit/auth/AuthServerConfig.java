70fd61a88110db814fe10daea69beac339a107ee
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-01-23 19:41:38 +0100

    Organized auth server code

--- auth-server/src/main/java/pro/shapeit/auth/AuthServerConfig.java
+++ auth-server/src/main/java/pro/shapeit/auth/AuthServerConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -24,6 +23,7 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import pro.shapeit.util.HttpUtils;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -53,44 +53,28 @@ public class AuthServerConfig {
 
   @Bean
   @Order(1)
-  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
-      throws Exception {
-    OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-        OAuth2AuthorizationServerConfigurer.authorizationServer();
+  public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    var authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer()
+        .oidc(withDefaults());
 
-    http
-        .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
-        .with(authorizationServerConfigurer, (authorizationServer) ->
-            authorizationServer
-                .oidc(Customizer.withDefaults())	// Enable OpenID Connect 1.0
+    http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher());
+    http.with(authorizationServerConfigurer, withDefaults());
+    http.authorizeHttpRequests(HttpUtils::anyAuthenticated);
+    http.exceptionHandling(exceptions -> exceptions
+        .defaultAuthenticationEntryPointFor(
+            new LoginUrlAuthenticationEntryPoint("/login"),
+            new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
         )
-        .authorizeHttpRequests((authorize) ->
-            authorize
-                .anyRequest().authenticated()
-        )
-        // Redirect to the login page when not authenticated from the
-        // authorization endpoint
-        .exceptionHandling((exceptions) -> exceptions
-            .defaultAuthenticationEntryPointFor(
-                new LoginUrlAuthenticationEntryPoint("/login"),
-                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-            )
-        );
+    );
 
     return http.build();
   }
 
   @Bean
   @Order(2)
-  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-      throws Exception {
-    http
-        .authorizeHttpRequests((authorize) -> authorize
-            .anyRequest().authenticated()
-        )
-        // Form login handles the redirect to the login page from the
-        // authorization server filter chain
-        .formLogin(Customizer.withDefaults());
+  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(HttpUtils::anyAuthenticated);
+    http.formLogin(withDefaults());
 
     return http.build();
   }
