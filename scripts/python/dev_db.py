13db717943e88b2f67ab56e2aa6fc976307dfb7b
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-02-28 05:47:02 +0100

    Created docker compose for training service

--- scripts/python/dev_db.py
+++ scripts/python/dev_db.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run(["docker", "compose", "-f" , "../../docker/compose/dev/db.yml", "up"])
+subprocess.run(["docker", "compose", "-f" , "../../docker/compose/dev/db.yml-docker-compose", "up"])
