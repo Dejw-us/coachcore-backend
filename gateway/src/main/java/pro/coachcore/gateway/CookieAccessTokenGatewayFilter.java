1e6df6b68b9a6f94cf4668b525315567c5465229
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-02-28 04:30:56 +0100

    Organized docker folder

--- gateway/src/main/java/pro/coachcore/gateway/CookieAccessTokenGatewayFilter.java
+++ gateway/src/main/java/pro/coachcore/gateway/CookieAccessTokenGatewayFilter.java
@@ -3,15 +3,19 @@ package pro.coachcore.gateway;
 import lombok.extern.slf4j.Slf4j;
 import pro.coachcore.oauth2.common.CookieTokensNames;
 
+import java.util.function.Function;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.http.HttpHeaders;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
+import org.springframework.http.HttpHeaders; 
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
 @Slf4j
+@Component
 public class CookieAccessTokenGatewayFilter implements GatewayFilter {
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -19,16 +23,22 @@ public class CookieAccessTokenGatewayFilter implements GatewayFilter {
     var accessTokenCookie = cookies.getFirst(CookieTokensNames.ACCESS_TOKEN);
 
     if (accessTokenCookie == null) {
+      log.debug("No access token cookie found");
       return chain.filter(exchange);
     }
-
+    log.debug("Found access token cookie");
     var modifiedRequest = exchange.getRequest().mutate()
         .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(accessTokenCookie.getValue()))
         .build();
+    log.debug("Added access token header");
     var modifiedExchange = exchange.mutate()
         .request(modifiedRequest)
         .build();
 
     return chain.filter(modifiedExchange);
+  }
+
+  public Function<GatewayFilterSpec, UriSpec> asFunctionFilter() {
+    return filters -> filters.filter(this);
   }
 }
--- gateway/src/main/java/pro/coachcore/gateway/GatewayConfig.java
+++ gateway/src/main/java/pro/coachcore/gateway/GatewayConfig.java
@@ -26,13 +26,20 @@ public class GatewayConfig {
   RouteLocator routeLocator(RouteLocatorBuilder builder) {
     return builder.routes()
         // .route("training-plans", configureTrainingApiRoute("/training-plans/**"))
-        // .route("catalog-exercises", configureTrainingApiRoute("/catalog-exercises/**"))
-        // .route("exercise-categories", configureTrainingApiRoute("/exercise-categories/**"))
+        // .route("catalog-exercises",
+        // configureTrainingApiRoute("/catalog-exercises/**"))
+        // .route("exercise-categories",
+        // configureTrainingApiRoute("/exercise-categories/**"))
         .route("oauth2-refresh-token", route -> route
             .path("/oauth2/token")
             .filters(filters -> filters.filter(cookieRefreshTokenGatewayFilter))
             .uri(servicesProperites.oauth2ServerUrl()))
-        // .route("users", configureApiRoute("/users/**", servicesProperites.oauth2ServerUrl()))
+        .route("newsletter-service", route -> route
+            .path("/newsletter/**")
+            .filters(cookieTokenGatewayFilter.asFunctionFilter())
+            .uri(servicesProperites.newsletterServiceUrl()))
+        // .route("users", configureApiRoute("/users/**",
+        // servicesProperites.oauth2ServerUrl()))
         .build();
   }
 
